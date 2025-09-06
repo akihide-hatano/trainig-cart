a1a6f209c90f475bc4036ec9181473fff0407bcc
akihide-hatano <a.h.19841202@gmail.com>
2025-09-06 16:57:21 +0900

    AuthController.phpを修正しました。

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    //
+    public function showLoginForm(){
+        return view('auth.login');
+    }
+
+    // ログイン処理
+    public function login(Request $request)
+    {
+        // ここは次のステップで実装！
+    }
+
+    //ログアウト処理
+    public function logout(Request $request){
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('/login');
+    }
 }
