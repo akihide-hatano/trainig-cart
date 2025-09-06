d08f6cc14634d103dc7dbe44e9831eb23b9e4ac6
akihide-hatano <a.h.19841202@gmail.com>
2025-09-06 16:53:12 +0900

    web.phpを編集して、AuthControllerをルーティングに追加しました。

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+//geustならlogin画面
+Route::middleware('guest')->group(function(){
+    Route::get('/login',[AuthController::class,'showLoginForm'])->name('login.form');
+    Route::post('login',[AuthController::class,'login'])->name('login');
+});
+
+//承認したらユーザーにdashboadを見せる。
+Route::middleware('auth')->group(function(){
+    Route::get('/dashboard', fn() => view('dashboard'))->name('dashboard');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 });
