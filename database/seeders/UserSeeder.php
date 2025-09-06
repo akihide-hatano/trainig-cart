49dc09ed3cb0eb60ccbb1969064aa3f6f2b7dcd7
akihide-hatano <a.h.19841202@gmail.com>
2025-09-06 09:38:26 +0900

    UserSeederとdatabaseSeederを修正しました。

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,8 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+
+use Faker\Factory as Faker;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +17,19 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        User::create([
+            'name'=>'test',
+            'email'=>'test@example.com',
+            'password'=>Hash::make('password'),
+        ]);
+
+        $faker = Faker::create('ja_JP'); // 日本語ロケール
+        for ($i = 0; $i < 5; $i++) {
+        User::create([
+            'name' => $faker->name,
+            'email' => $faker->unique()->safeEmail,
+            'password' => Hash::make('password'), // 全員同じパスワードにしておくとテストしやすい
+        ]);
+        }
     }
 }
