2b8726829604aeaeff90bd8a88ac0e1c9c39c3b3
akihide-hatano <a.h.19841202@gmail.com>
2025-09-08 10:39:33 +0900

    CartItemのmodelを作成

--- app/Models/CartItem.php
+++ app/Models/CartItem.php
@@ -2,9 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class CartItem extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'product_id',
+        'quantity',
+        'unit_price',
+    ];
+
+    /**
+     * Get the product that the cart item belongs to.
+     */
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
