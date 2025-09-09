e75664d6df5de245f5463071ade49162b23da7ab
akihide-hatano <a.h.19841202@gmail.com>
2025-09-09 15:28:43 +0900

    OrderItemSeederを合計を計算するように修正

--- database/seeders/OrderItemSeeder.php
+++ database/seeders/OrderItemSeeder.php
@@ -21,19 +21,24 @@ class OrderItemSeeder extends Seeder
         foreach ($orders as $order) {
             // 各注文に 1〜3 個の商品を紐づける
             $selectedProducts = $products->random(rand(1, 3));
+            $total = 0;
 
             foreach ($selectedProducts as $product) {
                 $qty   = rand(1, 5);
                 $price = $product->price;
+                $subtotal = $price * $qty;
 
                 OrderItem::create([
                     'order_id'   => $order->id,
                     'product_id' => $product->id,
                     'unit_price' => $price,
                     'quantity'   => $qty,
-                    'subtotal'   => $price * $qty,
+                    'subtotal'   => $subtotal,
                 ]);
+                $total += $subtotal;
             }
+            // Order の合計を更新
+            $order->update(['total_amount' => $total]);
         }
     }
 }
