4fd046cbd91a8889e984a67406434fd8e87462e3
akihide-hatano <a.h.19841202@gmail.com>
2025-09-09 15:07:08 +0900

    OrderSeeder.phpを10件に変更しました

--- database/seeders/OrderSeeder.php
+++ database/seeders/OrderSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Order;
 
 class OrderSeeder extends Seeder
 {
@@ -12,6 +13,14 @@ class OrderSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // 10件分の注文データを作成
+        for ($i = 1; $i <= 10; $i++){
+            Order::create([
+                'user_id'      => rand(1, 3), // 1〜3のユーザーIDをランダムで割り当て
+                'total_amount' => rand(3000, 20000), // 3000〜20000円
+                'status'       => $i % 2 === 0 ? 'paid' : 'pending',
+                'placed_at'    => now()->subDays(rand(0, 30)),
+            ]);
+        }
     }
 }
