debe817022adca32de17623e4e4c1f89b2bb113b
akihide-hatano <a.h.19841202@gmail.com>
2025-09-09 13:42:51 +0900

    Models/OrderItem.phpを更新しました

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -3,8 +3,30 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class OrderItem extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'order_id',
+        'product_id',
+        'unit_price',
+        'quantity',
+        'subtotal',
+    ];
+
+    protected $casts = [
+        'order_id'   => 'integer',
+        'product_id' => 'integer',
+    ];
+
+    public function order(){
+        return $this->belongsTo(Order::class);
+    }
+
+        public function product() {
+        return $this->belongsTo(Product::class);
+    }
 }
