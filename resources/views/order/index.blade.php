ca92f8db4042ecb756b3022b585b75fa8d1f3dd4
akihide-hatano <a.h.19841202@gmail.com>
2025-09-10 14:19:27 +0900

    order/index.blade.phpの詳細buttonの色を変更

--- resources/views/order/index.blade.php
+++ resources/views/order/index.blade.php
@@ -32,7 +32,10 @@
                         <x-status-badge :status="$order->status" />
                     </td>
                     <td class="px-4 py-2">
-                      <a href="{{ route('orders.show', $order) }}" class="text-indigo-600 hover:underline">詳細</a>
+                            <a href="{{ route('orders.show', $order) }}"
+                                class="inline-flex items-center px-3 py-1 bg-blue-500 text-white text-sm font-semibold rounded hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition">
+                                詳細
+                            </a>
                     </td>
                   </tr>
                 @endforeach
