8b0102b7d42ebc357c2258a41fc35d12dc69c979
akihide-hatano <a.h.19841202@gmail.com>
2025-09-10 14:38:20 +0900

    order/index.blade.phpを修正 詳細と削除機能を追加

--- resources/views/order/index.blade.php
+++ resources/views/order/index.blade.php
@@ -19,6 +19,7 @@
                   <th class="px-4 py-2 text-left">商品数</th>
                   <th class="px-4 py-2 text-left">合計金額</th>
                   <th class="px-4 py-2 text-left">状態</th>
+                  <th class="px-4 py-2 text-left">操作</th>
                   <th class="px-4 py-2"></th>
                 </tr>
               </thead>
@@ -31,11 +32,21 @@
                     <td class="px-4 py-2">
                         <x-status-badge :status="$order->status" />
                     </td>
-                    <td class="px-4 py-2">
-                            <a href="{{ route('orders.show', $order) }}"
-                                class="inline-flex items-center px-3 py-1 bg-blue-500 text-white text-sm font-semibold rounded hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition">
-                                詳細
-                            </a>
+                    <td class="py-2">
+                    <a href="{{ route('orders.show', $order) }}"
+                        class="inline-flex items-center px-3 py-1 bg-blue-500 text-white text-sm font-semibold rounded hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition">
+                        詳細
+                    </a>
+                    {{-- 削除ボタン（赤） --}}
+                    <form action="{{ route('orders.destroy', $order) }}" method="POST" class="inline">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit"
+                            onclick="return confirm('本当に削除しますか？')"
+                            class="inline-flex items-center px-3 py-1 bg-red-600 text-white text-sm font-semibold rounded hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition">
+                            削除
+                        </button>
+                    </form>
                     </td>
                 </tr>
                 @endforeach
