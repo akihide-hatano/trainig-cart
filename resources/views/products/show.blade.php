42aabb0bf1f1ff344ffcf6d149f4a29550d501cb
akihide-hatano <a.h.19841202@gmail.com>
2025-09-11 14:30:07 +0900

    flash/message.blade.phpに変更し,flash-message.blade.phpを使用している箇所を修正

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -6,9 +6,8 @@
     </x-slot>
 
     {{-- フラッシュメッセージ --}}
-    <x-flash-message />
-
-
+    <x-flash.message />
+    
 <div class="py-10">
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="bg-white shadow-sm sm:rounded-lg p-6">
