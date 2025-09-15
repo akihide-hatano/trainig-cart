2e779b2a586f65d9efe0796e7599c25402b82087
akihide-hatano <a.h.19841202@gmail.com>
2025-09-15 18:30:59 +0900

    dashboard.blade.phpを修正

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -119,10 +119,19 @@
       </article>
     @endforeach
   </div>
-  @vite('resources/js/dashboard.js')
+  <div class="mt-8 text-center">
+    <a href="{{ route('products.index') }}"
+        class="inline-flex items-center px-8 py-3 bg-green-600 text-white text-lg font-bold rounded-full shadow-lg
+        hover:bg-green-700 hover:shadow-xl transform hover:scale-105 transition ease-in-out duration-300">
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+</svg>
+
+        商品一覧へ
+    </a>
+</div>
 </section>
-
-
-    </div>
-  </div>
+</div>
+</div>
+@vite('resources/js/dashboard.js')
 </x-app-layout>
